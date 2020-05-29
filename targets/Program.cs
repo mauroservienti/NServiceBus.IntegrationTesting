f778da3b6f460b08bddb8b4184aeab42b70be75d
mauroservienti <mauro.servienti@gmail.com>
2020-05-29 09:35:01 +0200

    Cleanup

--- targets/Program.cs
+++ targets/Program.cs
@@ -1,6 +1,4 @@
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
 
@@ -9,7 +7,6 @@ internal class Program
     public static void Main(string[] args)
     {
         var sourceDir = "src";
-        //var dockerComposeYmlFullPath = Path.GetFullPath(Directory.EnumerateFiles(sourceDir, "docker-compose.yml", SearchOption.AllDirectories).Single());
 
         var sdk = new DotnetSdkManager();
 
@@ -29,9 +26,9 @@ internal class Program
                 var dockerComposeYmlFullPath = proj.Replace(projFileName, dockerComposeYmlFileName);
 
 
-                Run("docker-compose", @$"up -f \"{dockerComposeYmlFullPath}\" -d");
+                Run("docker-compose", $"up -f \"{dockerComposeYmlFullPath}\" -d");
                 Run(sdk.GetDotnetCliPath(), $"test \"{proj}\" --configuration Release --no-build");
-                Run("docker-compose", @$"down -f \"{dockerComposeYmlFullPath}\"");
+                Run("docker-compose", $"down -f \"{dockerComposeYmlFullPath}\"");
             });
 
         RunTargetsAndExit(args);
