573c8f7ca0570d223acd5db7e734478d622e17d5
Mauro Servienti <mauro.servienti@gmail.com>
2020-06-01 16:12:36 +0200

    Add first outgoing messages test

---
+++ src/NServiceBus.IntegrationTesting.Tests/Send_Operation_Interceptor.cs
@@ -0,0 +1,29 @@
+﻿using MyMessages.Messages;
+using NServiceBus.Pipeline;
+using NServiceBus.Testing;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NServiceBus.IntegrationTesting.Tests
+{
+    public class Send_Operation_Interceptor
+    {
+        [Test]
+        public async Task Should_Capture_Sent_Message_Operation()
+        {
+            var integrationContext = new IntegrationScenarioContext();
+            
+            var sut = new InterceptSendOperations("fake-endpoint", integrationContext); ;
+            var context = new TestableOutgoingSendContext
+            {
+                Message = new OutgoingLogicalMessage(typeof(AMessage), new AMessage())
+            };
+
+            await sut.Invoke(context, () => Task.CompletedTask).ConfigureAwait(false);
+
+            Assert.AreEqual(1, integrationContext.OutgoingMessageOperations.Count());
+        }
+    }
+}
