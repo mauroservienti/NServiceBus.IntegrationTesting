2b274607473ec2c17e39e01f662d0764e4b84fb6
mauroservienti <mauro.servienti@gmail.com>
2020-06-02 10:49:27 +0200

    Trigger API

---
+++ src/NServiceBus.IntegrationTesting/MessageSessionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace NServiceBus.IntegrationTesting
+{
+    public static class MessageSessionExtensions
+    {
+        public static Task TriggerTimeoutFor<TSaga>(this IMessageSession messageSession, string destinationEndpointName, string sagaUniqueIdentifier, object timeoutInstance) 
+        {
+            var sendOptions = new SendOptions();
+            sendOptions.SetDestination(destinationEndpointName);
+            sendOptions.SetHeader(Headers.SagaType, typeof(TSaga).AssemblyQualifiedName);
+            sendOptions.SetHeader(Headers.IsSagaTimeoutMessage, bool.TrueString);
+            sendOptions.SetHeader(Headers.SagaId, sagaUniqueIdentifier);
+
+            return messageSession.Send(timeoutInstance, sendOptions);
+        }
+    }
+}
