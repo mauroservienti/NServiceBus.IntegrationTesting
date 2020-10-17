5767158e41c7f690e3c528afc6057ec985ff01da
mauroservienti <mauro.servienti@gmail.com>
2020-10-17 11:15:23 +0200

    Approved API support

---
+++ src/NServiceBus.IntegrationTesting.Tests/API/ApiApprovals.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NServiceBus.IntegrationTesting;
+using NUnit.Framework;
+using PublicApiGenerator;
+
+namespace NServiceBus.Extensions.EndpointStarted.Tests.API
+{
+    public class APIApprovals
+    {
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Approve_API()
+        {
+            var publicApi = typeof(IntegrationScenarioContext).Assembly.GeneratePublicApi();
+            Approvals.Verify(publicApi);
+        }
+    }
+}
