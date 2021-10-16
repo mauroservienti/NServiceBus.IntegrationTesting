d261933888a674d1b1342da38d52181e03c1516e
Mauro Servienti <mauro.servienti@gmail.com>
2021-10-16 18:37:31 +0200

    Add test proxy endpoint

---
+++ src/MyService.TestProxy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using Serilog;
+
+namespace MyService.TestProxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = MyService.Program.CreateHostBuilder(args, configEndpointForTests => { });
+
+            return builder;
+        }
+    }
+}
