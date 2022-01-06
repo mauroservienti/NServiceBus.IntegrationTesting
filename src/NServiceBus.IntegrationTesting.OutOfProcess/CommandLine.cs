9022416f598c155995c0a11002a64f419193bd78
Mauro Servienti <mauro.servienti@gmail.com>
2022-01-06 11:01:00 +0100

    Simplify dealing with command line arguments

---
+++ src/NServiceBus.IntegrationTesting.OutOfProcess/CommandLine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NServiceBus.IntegrationTesting.OutOfProcess
+{
+    public static class CommandLine
+    {
+        public static void EnsureIsIntegrationTest()
+        {
+            var integrationTest = Environment.CommandLine
+                .Split(' ')
+                .Where(x => x == "--integrationTest")
+                .SingleOrDefault();
+
+            if (string.IsNullOrWhiteSpace(integrationTest))
+            {
+                throw new ArgumentException("This library is designed for integration tests usage. If this is not an integration test remove the library dependency.");
+            }
+        }
+
+        public static string GetEndpointName()
+        {
+            var endpointName = Environment.CommandLine
+                .Split(' ')
+                .Where(x => x.StartsWith("--endpointName="))
+                .Single()
+                .Split('=')
+                .Last();
+
+            return endpointName;
+        }
+    }
+}
