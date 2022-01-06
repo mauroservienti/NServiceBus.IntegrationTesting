2499e706197e3cb0bde130d3449d2de21f66e1f8
Mauro Servienti <mauro.servienti@gmail.com>
2022-01-06 10:51:59 +0100

    Shortcut the test proxy startup code

--- src/MyService.TestProxy/Program.cs
+++ src/MyService.TestProxy/Program.cs
@@ -9,23 +9,7 @@ namespace MyService.TestProxy
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            foreach (var item in args)
-            {
-                Console.WriteLine("Argument: " + item);
-            }
-
-            var builder = MyService.Program.CreateHostBuilder(args, endpointConfiguration => 
-            {
-                //TODO: get the endpoint name from command line args
-                endpointConfiguration.EnableOutOfProcessIntegrationTesting("MyService");
-            });
-
-            return builder;
+            MyService.Program.CreateHostBuilder(args).Build().Run();
         }
     }
 }
