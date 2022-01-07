80cbad661c4928b64f67290bca10fdd04b0d2311
Mauro Servienti <mauro.servienti@gmail.com>
2022-01-07 05:38:53 +0100

    in process and out of process in the same test

--- src/MySystem.AcceptanceTests/When_sending_AMessage.cs
+++ src/MySystem.AcceptanceTests/When_sending_AMessage.cs
@@ -1,57 +1,81 @@
-//using MyMessages.Messages;
-//using NServiceBus;
-//using NServiceBus.AcceptanceTesting;
-//using NServiceBus.IntegrationTesting;
-//using NUnit.Framework;
-//using System;
-//using System.Linq;
-//using System.Threading.Tasks;
+using MyMessages.Messages;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NServiceBus.IntegrationTesting;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace MySystem.AcceptanceTests
-//{
-//    public class When_sending_AMessage
-//    {
-//        //TODO: renable when a compatible combination of alphas is available
-//        //[OneTimeSetUp]
-//        //public async Task Setup()
-//        //{
-//        //    await DockerCompose.Up();
-//        //}
+namespace MySystem.AcceptanceTests
+{
+    public class When_sending_AMessage
+    {
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            await DockerCompose.Up();
+        }
 
-//        //[OneTimeTearDown]
-//        //public void Teardown()
-//        //{
-//        //    DockerCompose.Down();
-//        //}
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            DockerCompose.Down();
+        }
 
-//        [Test]
-//        public async Task AReplyMessage_is_received_and_ASaga_is_started()
-//        {
-//            var theExpectedIdentifier = Guid.NewGuid();
-//            var context = await Scenario.Define<IntegrationScenarioContext>()
-//                .WithGenericHostEndpoint("MyService", configPreview => Program.CreateHostBuilder(new string[0], configPreview).Build(), behavior =>
-//                {
-//                    behavior.When(session => session.Send(new AMessage() {AnIdentifier = theExpectedIdentifier}));
-//                })
-//                .WithEndpoint<MyOtherServiceEndpoint>()
-//                .Done(c => c.SagaWasInvoked<ASaga>() || c.HasFailedMessages())
-//                .Run();
+        class Context : IntegrationScenarioContext
+        {
+            public bool RemoteWhenExecuted { get; set; }
+            public bool InProcessWhenExecuted { get; set; }
+        }
 
-//            var invokedSaga = context.InvokedSagas.Single(s => s.SagaType == typeof(ASaga));
+        [Test]
+        public async Task AReplyMessage_is_received_and_ASaga_is_started()
+        {
+            var theExpectedIdentifier = Guid.NewGuid();
+            var context = await Scenario.Define<Context>()
+                .WithOutOfProcessEndpoint("MyService", EndpointReference.FromSolutionProject("MyService.TestProxy"), behavior =>
+                {
+                    behavior.When((remoteSession, ctx) =>
+                    {
+                        ctx.RemoteWhenExecuted = true;
+                        return Task.CompletedTask;
+                    });
+                    //behavior.When(session => session.Send(new AMessage() { AnIdentifier = theExpectedIdentifier }));
+                })
+                .WithEndpoint<MyOtherServiceEndpoint>(behavior => 
+                {
+                    behavior.When((session, ctx) =>
+                    {
+                        ctx.InProcessWhenExecuted = true;
+                        return Task.CompletedTask;
+                    });
+                })
+                .Done(c =>
+                {
+                    return c.RemoteWhenExecuted && c.InProcessWhenExecuted;
+                    //return c.SagaWasInvoked<ASaga>() || c.HasFailedMessages();
+                })
+                .Run();
 
-//            Assert.True(invokedSaga.IsNew);
-//            Assert.AreEqual("MyService", invokedSaga.EndpointName);
-//            Assert.True(((ASagaData)invokedSaga.SagaData).AnIdentifier == theExpectedIdentifier);
-//            Assert.False(context.HasFailedMessages());
-//            Assert.False(context.HasHandlingErrors());
-//        }
+            Assert.True(context.RemoteWhenExecuted);
+            Assert.True(context.InProcessWhenExecuted);
 
-//        class MyOtherServiceEndpoint : EndpointConfigurationBuilder
-//        {
-//            public MyOtherServiceEndpoint()
-//            {
-//                EndpointSetup<MyOtherServiceTemplate>();
-//            }
-//        }
-//    }
-//}
+            //var invokedSaga = context.InvokedSagas.Single(s => s.SagaType == typeof(ASaga));
+
+            //Assert.True(invokedSaga.IsNew);
+            //Assert.AreEqual("MyService", invokedSaga.EndpointName);
+            //Assert.True(((ASagaData)invokedSaga.SagaData).AnIdentifier == theExpectedIdentifier);
+            Assert.False(context.HasFailedMessages());
+            Assert.False(context.HasHandlingErrors());
+        }
+
+        class MyOtherServiceEndpoint : EndpointConfigurationBuilder
+        {
+            public MyOtherServiceEndpoint()
+            {
+                EndpointSetup<MyOtherServiceTemplate>();
+            }
+        }
+    }
+}
