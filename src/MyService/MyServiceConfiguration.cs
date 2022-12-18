247bbb2778165bed30f1d933f9de94e844d3fc9e
Mauro Servienti <mauro.servienti@gmail.com>
2022-12-18 21:28:45 +0100

    Re-enable Rabbit transport

--- src/MyService/MyServiceConfiguration.cs
+++ src/MyService/MyServiceConfiguration.cs
@@ -15,11 +15,10 @@ namespace MyService
             this.UsePersistence<LearningPersistence>();
             this.EnableInstallers();
 
-            //TODO: renable when a compatible combination of alphas is available
-            //var transport = new RabbitMQTransport(Topology.Conventional, "host=localhost;username=guest;password=guest");
-            //var routing = this.UseTransport(transport);
-
-            var routing = this.UseTransport(new LearningTransport());
+            var transport = new RabbitMQTransport(
+                RoutingTopology.Conventional(QueueType.Quorum),
+                "host=localhost;username=guest;password=guest");
+            var routing = this.UseTransport(transport);
 
             routing.RouteToEndpoint(typeof(AMessage), "MyOtherService");
 
